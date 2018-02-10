fa76acd517dd3255a928b4b434dc4c8b29235a6f
RandyStevens <Randy@momentum.io>
2018-02-09 20:09:20 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# tao_scrape
+
+Created from templates made available by Stagehand under a BSD-style
+[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
+
+## Usage
+Clone into your working directory then:
+    
+    cd /tao_scrape
+    
+run:
+    
+    pub get
+
+
+A simple usage example:
+
+    dart tao_scrape_example.dart
---
+++ example/tao_scrape_example.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:html/parser.dart';
+import 'package:html/dom.dart';
+
+main() {
+  final url = 'http://www.mit.edu/~xela/tao.html';
+
+  getHtml(url).then((document) {
+    // page title
+    print(document.querySelector('title').text);
+    //remove the 'Where i got this text'
+    document.querySelector('p').remove();
+    //Get the length of our document
+    for (var p = 0; p < document.querySelectorAll('p').length; p+1) {
+      //print the text from the p tag
+      print(document.querySelector('p').text);
+      //remove the first p tag before the loop ends
+      document.querySelector('p').remove();
+    }
+  });
+}
+/// fetch and parse the HTML from [url]
+///
+Future<Document> getHtml(String url) =>
+    new HttpClient()
+        .getUrl(Uri.parse(url))
+        .then((req) => req.close())
+        .then((res) => res
+        .asyncExpand((bytes) => new Stream.fromIterable(bytes))
+        .toList())
+        .then((bytes) => parse(bytes, sourceUrl: url));
